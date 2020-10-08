bf2a4efdbe5487c5e670e2bf8065e5fa237874c1
Jonathan Valderrama <jvalderrama91@knights.ucf.edu>
2020-10-07 22:43:26 -0400

    Created new unity project
    Created new unity project to test out observer pattern.

---
+++ DesignPatterns/Assets/Flyweight Pattern/Scripts/PerlinPositionProxy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+using Unity.Mathematics;
+using Unity.Entities;
+
+[RequiresEntityConversion]
+public class PerlinPositionProxy : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var data = new PerlinPosition { };
+        dstManager.AddComponentData(entity, data);
+    }
+}
---
+++ DesignPatterns/Assets/Flyweight Pattern/Scripts/SpawnerSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+public class SpawnerSystem : JobComponentSystem
+{
+
+    EndSimulationEntityCommandBufferSystem _entityCommandBufferSystem;
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnCreate()
+    {
+        _entityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+}
