795692e56096716d914999f93114224a96747976
Jonathan Valderrama <jvalderrama91@knights.ucf.edu>
2020-10-20 18:46:51 -0400

    Testing operation walmart

---
+++ ObserverPattern/Assets/Radar/EventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityGameObjectEvent : UnityEvent<GameObject> { }
+
+public class EventListener : MonoBehaviour
+{
+    public Event gEvent;
+    public UnityGameObjectEvent response = new UnityGameObjectEvent();
+
+    private void OnEnable()
+    {
+        gEvent.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        gEvent.Unregister(this);
+    }
+
+    public void OnEventOccurs(GameObject go)
+    {
+        response.Invoke(go);
+    }
+}
